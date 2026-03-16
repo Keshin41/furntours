262d0c99f9e24f5f48eabe3d8f8c546bf7a6f42c
Saint-ouen Jimmy <jimmy.saintouen@gmail.com>
2026-03-16 20:09:44 +0100

    feat(contact): Add contact module and empty controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { FurmeetModule } from './furmeet/furmeet.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { UserModule } from './user/user.module';
     AuthModule,
     UserModule,
     FurmeetModule,
+    ContactModule,
   ],
   controllers: [AppController],
   providers: [AppService],
