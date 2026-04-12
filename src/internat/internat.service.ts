298f970f5b9eeeacdfb7e0745d8370ca181de0bf
Jimmy Saint-ouen <jimmy.saintouen@gmail.com>
2026-04-12 15:30:56 +0200

    feat(internat): update ticket shema + add backend business logic

--- src/internat/internat.service.ts
+++ src/internat/internat.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from '@nestjs/common';
-import { map } from 'rxjs';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 export interface TicketDTO {
@@ -7,13 +6,15 @@ export interface TicketDTO {
   firstname: string;
   nickname: string;
   email: string;
-  option: boolean;
-  payeur: boolean;
+  drap: boolean;
+  goodies: boolean;
 }
 
 @Injectable()
 export class InternatService {
   constructor(private prismaService: PrismaService){}
+
+  //todo: gestion stock
 
   manageTest = async (data: any) => {
     const items = data.items as TicketDTO[];
@@ -24,33 +25,61 @@ export class InternatService {
       // creer un orderItem
       // creer un ticket
     //paiement
+    
+    const produitInternat = await this.prismaService.product.findFirst({
+      where: { skus: {
+        some: {
+          skuCode: 'INTERNAT_2026',
+        }
+      }},
+      include: { skus: true },
+    });
 
-    // Fetch internat skus 2026
-    const skuInternatBasic = await this.prismaService.sku.findUnique({
-      where: { id: ''}
+    const skuAdhesion = await this.prismaService.sku.findUnique({
+      where: {
+        skuCode: 'ADHESION_2026',
+      },
+      include: { product: true },
     });
-    const skuInternatOptionDrap = await this.prismaService.sku.findUnique({
-      where: { id: ''}
-    });
-    const skuInternatBasicAdherent = await this.prismaService.sku.findUnique({
-      where: { id: ''}
-    });
-    const skuInternatOptionDrapAdherent = await this.prismaService.sku.findUnique({
-      where: { id: ''}
-    });
+
+    if (skuAdhesion == null || produitInternat == null ) {
+      throw new HttpException('Could not find products', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    const skuInternatNoDrapNoGoodies = produitInternat.skus.find((sku) => {sku.skuCode === ''});
+    const skuInternatDrapNoGoodies = produitInternat.skus.find((sku) => {sku.skuCode === ''});
+    const skuInternatNoDrapGoodies = produitInternat.skus.find((sku) => {sku.skuCode === ''});
+    const skuInternatDrapGoodies = produitInternat.skus.find((sku) => {sku.skuCode === ''});
+
+    if (skuInternatDrapGoodies == null || skuInternatDrapNoGoodies == null || skuInternatNoDrapGoodies == null || skuInternatNoDrapNoGoodies == null) {
+      throw new HttpException('Could not find products', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     // Create mapOrder (for orderItems)
-    const mapOrder = [
-      {type: 'basic', value: 0},
-      {type: 'drap', value: 0},
-      {type: 'basicAdherent', value: 0},
-      {type: 'drapAdherent', value: 0},
+    const mapOrderItems = [
+      {type: 'noDrapNoGoodies', value: 0},
+      {type: 'drapNoGoodies', value: 0},
+      {type: 'drapGoodies', value: 0},
+      {type: 'noDrapGoodies', value: 0},
+      {type: 'adhesion', value: 0},
     ]
+
+    // Fetch buyer user
+    const buyer = await this.prismaService.user.upsert({
+      where: { email: items[0].email },
+      update: {},
+      create: {
+        email: items[0].email,
+        lastname: items[0].surname,
+        firstname: items[0].firstname,
+        nickname: items[0].nickname,
+      },
+    });
 
     // Create Order
     const order = await this.prismaService.order.create({
       data: {
-        userId: '',
+        userId: buyer.id,
       }
     });
 
@@ -67,73 +96,95 @@ export class InternatService {
           firstname: item.firstname,
           nickname: item.nickname,
         },
+        include: { orders: {
+          include: { orderItems: {
+            include: { sku: true }
+          } }
+        } }
       });
 
       // todo: Fetch adherent ()
-      const userAdherent = false;
+      const userAdherent = user.orders.some(order => {
+        order.orderItems.some(orderItem => {
+          orderItem.sku == skuAdhesion;
+        })
+      })
 
-      if (item.option) {
-        if (userAdherent) {
-          mapOrder[3].value += 1;
+      // Fill mapOrderItem
+      if (item.drap) {
+        if (item.goodies) {
+          mapOrderItems[2].value += 1;
         } else {
-          mapOrder[1].value += 1;
+          mapOrderItems[1].value += 1;
         }
       } else {
-        if (userAdherent) {
-          mapOrder[2].value += 1;
+        if (item.goodies) {
+          mapOrderItems[3].value += 1;
         } else {
-          mapOrder[0].value += 1;
+          mapOrderItems[0].value += 1;
         }
       }
+      if (!userAdherent) {
+        mapOrderItems[4].value += 1;
+      }
 
-      const ticket = await this.prismaService.ticket.create({
+      // Create ticket
+      await this.prismaService.ticket.create({
         data: {
           userId: user.id,
-          orderItemId: item.option ? skuInternatOptionDrap?.id : skuInternatBasic?.id,
-          //skuId: item.option ? skuInternatOptionDrap?.id : skuInternatBasic?.id,
+          skuId: item.drap ? (item.goodies ? skuInternatDrapGoodies.id : skuInternatDrapNoGoodies.id) : (item.goodies ? skuInternatNoDrapGoodies.id : skuInternatNoDrapNoGoodies.id),
         }
       })
     }
 
     // Create orderIds
 
-    if (mapOrder[0].value > 0)
+    if (mapOrderItems[0].value > 0)
       this.prismaService.orderItem.create({
         data: {
           orderId: order.id,
-          quantity: mapOrder[0].value,
-          skuId: skuInternatBasic.id,
-          unitPrice : skuInternatBasic?.priceOverride,
+          quantity: mapOrderItems[0].value,
+          skuId: skuInternatNoDrapNoGoodies.id,
+          unitPrice : skuInternatNoDrapNoGoodies.priceOverride ?? produitInternat.basePrice,
         }
       });
 
-    if (mapOrder[1].value > 0)
+    if (mapOrderItems[1].value > 0)
       this.prismaService.orderItem.create({
         data: {
           orderId: order.id,
-          quantity: mapOrder[1].value,
-          skuId: skuInternatOptionDrap.id,
-          unitPrice : skuInternatOptionDrap?.priceOverride,
+          quantity: mapOrderItems[1].value,
+          skuId: skuInternatDrapNoGoodies.id,
+          unitPrice : skuInternatDrapNoGoodies.priceOverride ?? produitInternat.basePrice,
         }
       });
 
-    if (mapOrder[2].value > 0)
+    if (mapOrderItems[2].value > 0)
       this.prismaService.orderItem.create({
         data: {
           orderId: order.id,
-          quantity: mapOrder[2].value,
-          skuId: skuInternatBasicAdherent.id,
-          unitPrice : skuInternatBasicAdherent?.priceOverride,
+          quantity: mapOrderItems[2].value,
+          skuId: skuInternatDrapGoodies.id,
+          unitPrice : skuInternatDrapGoodies.priceOverride ?? produitInternat.basePrice
         }
       });
 
-    if (mapOrder[3].value > 0)
+    if (mapOrderItems[3].value > 0)
       this.prismaService.orderItem.create({
         data: {
           orderId: order.id,
-          quantity: mapOrder[3].value,
-          skuId: skuInternatOptionDrapAdherent.id,
-          unitPrice : skuInternatOptionDrapAdherent?.priceOverride,
+          quantity: mapOrderItems[3].value,
+          skuId: skuInternatNoDrapGoodies.id,
+          unitPrice : skuInternatNoDrapGoodies.priceOverride ?? produitInternat.basePrice,
+        }
+      });
+    if (mapOrderItems[4].value > 0)
+      this.prismaService.orderItem.create({
+        data: {
+          orderId: order.id,
+          quantity: mapOrderItems[4].value,
+          skuId: skuAdhesion.id,
+          unitPrice : skuAdhesion.priceOverride ?? skuAdhesion.product.basePrice,
         }
       });
   }
