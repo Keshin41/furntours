1920af53d6eab172dd36f7e38dfe76084ab37cd0
Vikari <vgorgeon@gmail.com>
2026-04-15 20:54:23 +0200

    feat(admin): adding tickets admin endpoint

---
+++ src/internat/internat.utils.ts
@@ -0,0 +1,13 @@
+import { TicketListDto, TicketsWithUsersSkuOrder } from './internat.dto';
+
+export const mapTicketsToTicketListDto = (
+  tickets: TicketsWithUsersSkuOrder[],
+): TicketListDto[] => {
+  return tickets.map((ticket) => ({
+    email: ticket.user.email,
+    nickname: ticket.user.nickname,
+    date: ticket.order.createdAt,
+    goodies: ticket.sku.skuCode.includes('GOODIES'),
+    duvet: ticket.sku.skuCode.includes('DRAPS'),
+  }));
+};
