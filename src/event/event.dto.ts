e4c8a97a082df1e86420195e770b137ba4d35579
KochyOkami <kochyokami@gmail.com>
2026-04-17 19:34:07 +0200

    feat(event): add update functionality for meets and DTO validation

---
+++ src/event/event.dto.ts
@@ -0,0 +1,23 @@
+import { createZodDto } from 'nestjs-zod';
+import { EventPartType } from 'src/generated/prisma/enums';
+import { z } from 'zod';
+
+const EventActivitySchema = z.object({
+  title: z.string().trim().min(1),
+  description: z.string().trim().optional().default(''),
+  date: z.string().trim().min(1),
+  order: z.number().int().nonnegative().optional(),
+  type: z.enum(EventPartType).optional().default(EventPartType.OTHER),
+});
+
+const UpsertMeetSchema = z.object({
+  title: z.string().trim().min(1),
+  description: z.string().trim().optional().default(''),
+  imageUrl: z.string().trim().optional(),
+  published: z.boolean().default(false),
+  opened: z.boolean().default(false),
+  eventActivities: z.array(EventActivitySchema).default([]),
+});
+
+export class CreateMeetDto extends createZodDto(UpsertMeetSchema) {}
+export class UpdateMeetDto extends createZodDto(UpsertMeetSchema) {}
